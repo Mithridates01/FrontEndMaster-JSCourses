80472a2ce7317ce2a6aeedf13fa1bdaca2f9f30d
Mithridates01 <jclucas21@gmail.com>
2017-05-17 13:51:35 -0500

    solve with let i

--- code-test.js
+++ code-test.js
@@ -1,7 +1,5 @@
-for (var i=1; i<=5; i++) {
-  (function IFEE(i){  
-    setTimeout(function(){
-        console.log("i: " + i);
-    },i * 1000);
-  })(i)
+for (let i=1; i<=5; i++) {
+  setTimeout(function(){
+    console.log("i: " + i);
+  }, i * 1000);
 }
